90710da529e8a99f3993180200727fc1b491d418
Kris Reeves <kris.re@bbhmedia.com>
2014-12-11 17:10:58 -0500

    Rearrange all the things. This really deserves a major version bump but then I'd be starting at 2.0, but npm won't let me re-publish 1.0...

---
+++ index.js
@@ -0,0 +1,181 @@
+'use strict';
+
+var PassThrough = require('stream').PassThrough,
+    inherits = require('util').inherits;
+
+var SyslogStream = require('syslog-streams2'),
+    UnixStream = require('unix-socket-streams2'),
+    UdpStream = require('udp-streams2'),
+    TcpStream = require('net').Socket;
+
+var clone = require('clone');
+
+function Syslog(opts) {
+    PassThrough.call(this, { objectMode: true });
+    
+    
+    this.shuttingDown = false;
+    this.pause();
+    
+    Syslog.parseArgs.call(this, opts);
+};
+inherits(Syslog, PassThrough);
+
+Syslog.create = function (opts, cb) {
+    if (typeof opts === 'function') {
+        cb = opts;
+        opts = null;
+    }
+
+    var stream = new Syslog(opts);
+    stream.connect(cb);
+    return stream;
+};
+
+Syslog.parseArgs = function (opts) {
+    opts = clone(opts || { });
+    
+    var conn = opts.connection || { };
+    delete opts.connection;
+    
+    // for unix domain sockets
+    this.path = conn.path || '/dev/log';
+    
+    // for tcp/udp sockets
+    this.host = conn.host || '127.0.0.1';
+    this.port = conn.port || 514;
+    
+    this.type = conn.type || (conn.path ? 'unix' : 'udp');
+    
+    this.syslog = new SyslogStream(opts);
+    this.stream = null;
+    this.pipe(this.syslog);
+    
+    var recon = (opts.reconnect && typeof opts.reconnect === 'object') ? opts.reconnect : { };
+    delete opts.reconnect;
+    
+    var num;
+    if (!recon.hasOwnProperty('enabled')) { recon.enabled = false; }
+    
+    num = parseInt(recon.maxTries, 10);
+    recon.maxTries = !isNaN(num) ? num : Infinity;
+    
+    num = parseInt(recon.initialDelay, 10);
+    recon.initialDelay = !isNaN(num) ? num : 100;
+    
+    num = parseInt(recon.delayFactor, 10);
+    recon.delayFactor = !isNaN(num) ? num : 1.2;
+    
+    num = parseInt(recon.maxDelay, 10);
+    recon.maxDelay = !isNaN(num) ? num : 30*1000;
+    
+    this.reconnect = recon; 
+};
+
+Syslog.prototype.end = function () {
+    this.shuttingDown = true;
+    Syslog.super_.prototype.end.apply(this, arguments);
+};
+Syslog.prototype.connect = function (cb) {
+    var self = this;
+    
+    switch (self.type) {
+        case 'unix':
+            self.stream = new UnixStream();
+            self.stream.connect(self.path);
+        break;
+        
+        case 'tcp':
+            self.stream = new TcpStream();
+            self.stream.connect(self.port, self.host);
+        break;
+        
+        case 'udp':
+            self.stream = new UdpStream();
+            self.stream.connect(self.port, self.host);
+        break;
+        
+        default:
+            cb(new Error('Unsupported transport type: ' + self.type));
+        break;
+    }
+    
+    var onConnect, onClose, onError, cleanup;
+    
+    var connected = false;
+    
+    onConnect = function () {
+        connected = true;
+        
+        self.syslog.pipe(self.stream);
+        self.resume();
+        
+        if (typeof cb === 'function') { cb(); }
+        else { self.emit('connect'); }
+    };
+    onClose = function () {
+        cleanup();
+    };
+    onError = function (err) {
+        // if it was a connection error, trigger the callback
+        if (!connected && typeof cb === 'function') {
+            cb(err);
+        }
+        
+        self.emit('warn', err);
+        
+        cleanup();
+    };
+    cleanup = function () {
+        self.pause();
+        self.unpipe(self.stream);
+        
+        self.stream.removeListener('error', onError);
+        self.stream.removeListener('connect', onConnect);
+        self.stream.removeListener('close', onClose);
+        
+        self.stream = null;
+        
+        self.maybeReconnect();
+    };
+    
+    // each stream only errors once, so we use '.once' instead of '.on' here
+    // even though we ourselves may emit multiple errors
+    self.stream.once('error', onError);
+    self.stream.once('connect', onConnect);
+    self.stream.once('close', onClose);
+    
+    return self;
+};
+Syslog.prototype.maybeReconnect = function () {
+    var self = this;
+    
+    if (self.stream) { return; }
+    
+    if (self.shuttingDown) { return; }
+    
+    var recon = self.reconnect;
+    if (!recon.enabled) {
+        self.emit('error', new Error('Disconnected, reconnect disabled'));
+        return;
+    }
+    
+    (function retry(n, dly) {
+        if (self.shuttingDown) { return; }
+        
+        if (n >= recon.maxTries) {
+            self.emit('error', new Error('Unable to reconnect, reached max retries'));
+            return;
+        }
+        
+        setTimeout(function () {
+            self.connect(function (err) {
+                if (err) {
+                    return retry(n+1, Math.min(recon.maxDelay, dly * recon.delayFactor));
+                }
+            });
+        }, dly);
+    })(0, recon.initialDelay);
+};
+
+module.exports = Syslog;
---
+++ test.js
@@ -0,0 +1,221 @@
+'use strict';
+
+var Syslog = require('./index'),
+    fs = require('fs'),
+    net = require('net'),
+    dgram = require('dgram');
+
+var BIND_PORT = 1234,
+    SOCKET_FILE = '/tmp/syslog2-test';
+
+require('should');
+
+function unlink(done) {
+    fs.unlink(SOCKET_FILE, done.bind(null, null));
+}
+before(unlink);
+after(unlink);
+
+// these are more thoroughly tested in their respective modules; just making sure
+// that the logic to hook them up works correctly
+describe('Transports', function () {
+    describe('tcp', function () {
+        var server;
+        
+        before(function (done) {
+            server = net.createServer();
+            server.listen(BIND_PORT, done);
+        });
+        after(function (done) {
+            server.close(done);
+        });
+        it('should connect and pass messages', function (done) {
+            var log = new Syslog({
+                connection: {
+                    type: 'tcp',
+                    host: '127.0.0.1',
+                    port: BIND_PORT
+                }
+            });
+            
+            server.once('connection', function (socket) {
+                socket.once('data', function (chunk) {
+                    log.end(done);
+                });
+            });
+            
+            log.connect(function () {
+                log.write({msg: 'hello'});
+            });
+        });
+    });
+    describe('unix socket', function () {
+        var server;
+        
+        before(function (done) {
+            server = net.createServer();
+            server.listen(SOCKET_FILE, done);
+        });
+        after(function (done) {
+            server.close(done);
+        });
+        it('should connect and pass messages', function (done) {
+            var log = new Syslog({
+                connection: {
+                    type: 'unix',
+                    path: SOCKET_FILE
+                }
+            });
+            
+            server.once('connection', function (socket) {
+                socket.once('data', function (chunk) {
+                    log.end(done);
+                });
+            });
+            
+            log.connect(function () {
+                log.write({msg: 'hello'});
+            });
+        });
+    });
+    describe('udp', function () {
+        var server;
+        
+        before(function (done) {
+            server = dgram.createSocket('udp4');
+            server.bind(BIND_PORT, done);
+        });
+        after(function () {
+            server.close();
+        });
+        it('should pass messages', function (done) {
+            var log = new Syslog({
+                connection: {
+                    type: 'udp',
+                    host: '127.0.0.1',
+                    port: BIND_PORT
+                }
+            });
+            
+            server.once('message', function (chunk) {
+                log.end(done);
+            });
+            
+            log.connect(function () {
+                log.write({msg: 'hello'});
+            });
+        });
+    });
+});
+
+describe('Syslog2', function () {
+    var server;
+    
+    beforeEach(function (done) {
+        server = net.createServer();
+        server.listen(BIND_PORT, done);
+    });
+    afterEach(function (done) {
+        server.close(done);
+    });
+
+    it('should default to not reconnecting', function (done) {
+        var log = new Syslog({
+            connection: {
+                type: 'tcp',
+                port: BIND_PORT
+            }
+        });
+        
+        server.once('connection', function (socket) {
+            socket.destroy();
+        });
+        
+        log.connect(function () {
+            log.once('error', function (err) {
+                err.message.should.match(/Disconnected, reconnect disabled/);
+                log.end(done);
+            });
+        });
+    });
+
+    it('should emit a \'warn\' event for socket errors while reconnecting', function (done) {
+        var log = new Syslog({
+            connection: {
+                type: 'tcp',
+                port: BIND_PORT
+            }, reconnect: {
+                enabled: true,
+                maxTries: 1,
+                initalDelay: 0,
+                delayFactor: 0,
+                maxDelay: 0
+            }
+        });
+        
+        server.once('connection', function (socket) {
+            log.stream.emit('error', 'foo');
+            socket.destroy();
+        });
+        
+        log.connect(function () {
+            log.once('warn', function (err) {
+                err.should.equal('foo');
+                log.end(done);
+            });
+        });
+    });
+    
+    it('should give up after the specified number of reconnection attempts', function (done) {
+        var log = new Syslog({
+            connection: {
+                type: 'tcp',
+                ip: '127.0.0.1',
+                port: 64993
+            }, reconnect: {
+                enabled: true,
+                maxTries: 1,
+                initalDelay: 0,
+                delayFactor: 0,
+                maxDelay: 0
+            }
+        });
+        
+        server.once('connection', function (socket) {
+            socket.destroy();
+        });
+        
+        log.connect(function () {
+            log.once('error', function (err) {
+                err.should.match(/Unable to reconnect, reach max retries/);
+                log.end(done);
+            });
+        });
+    });
+    
+    it('should buffer messages for delivery when connected', function (done) {
+        var log = new Syslog({
+            connection: {
+                type: 'tcp',
+                ip: '127.0.0.1',
+                port: BIND_PORT
+            }
+        });
+        
+        server.once('connection', function (socket) {
+            socket.once('data', function (chunk) {
+                log.end(done);
+            });
+        });
+        
+        log.on('warn', function (err) {
+            console.log(err.stack);
+        });
+        log.write({ msg: 'hello' });
+        log.connect();
+    });
+    
+    xit('should not lose messages on write errors', function (done) {
+        // implement the ring buffer thing and a protection layer rather than a direct pipe
+    });
+});
